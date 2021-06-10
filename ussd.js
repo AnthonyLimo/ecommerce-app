dcebb6e4a1b92c472d51754972e6efd247332d66
Anthony Kiplimo <anthony.kiplimo@africastalking.com>
2021-06-10 11:41:46 +0300

    Sample app

--- ussd.js
+++ ussd.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: false }));
 
 // Configure AT SDK
 const credentials = {
-    apiKey: "22925984ba507e551882375ad5ba766efe1a040217adb83a043651ac0eca44d1",
-    username: "sandbox"
+    apiKey: "",
+    username: ""
 };
 
 const AfricasTalking = require("africastalking")(credentials);
@@ -41,62 +41,21 @@ app.post("/ussd", (req, res) => {
     } else if ( text == "1*1*1" ) {
         response = "END Thanks for making the purchase! Please complete the purchase on the dialogue on your phone.";
         // TODO: Add Payments here
-        const options = {
-            productName: "DUKA",
-            phoneNumber: phoneNumber,
-            currencyCode: "KES",
-            amount: 500,
-            metadata: {
-                foo: "bar",
-                key: "value"
-            }
-        };
-        setTimeout(() => {
-            payments.mobileCheckout(options).catch(console.error);
-        }, 5000);
+
     } else if ( text == "1*2" ) {
         response = "CON This case costs KES 500. Would you like to complete the purchase?\n 1. Yes\n 2. No";
     } else if ( text == "1*2*1" ) {
         response = "END Thanks for making the purchase! Please complete the purchase on the dialogue on your phone.";
         // TODO: Add payments here
-        const options = {
-            productName: "DUKA",
-            phoneNumber: phoneNumber,
-            currencyCode: "KES",
-            amount: 500,
-            metadata: {
-                foo: "bar",
-                key: "value"
-            }
-        };
-        setTimeout(() => {
-            payments.mobileCheckout(options).catch(console.error);
-        }, 5000);
+
     } else if ( text == "2" ) {
         response = "END Sending your receipt in a moment to your SMS inbox";
         // Add SMS functionality here
-        const options = {
-            message: "Hey there!",
-            to: phoneNumber
-        };
-        setTimeout(() => {
-            // Add SMS code here
-            sms.send(options).catch(console.error);
-        });
+
     } else if ( text == "3" ) {
         response = "END We've sent you some airtime as a reward. Thanks for being a loyal customer!";
         // TODO: Add Airtime functionality here
-        const options = {
-            recipients: [{
-                phoneNumber: phoneNumber,
-                currencyCode: "KES",
-                amount: "100"
-            }]
-        };
-        setTimeout(() => {
-            // Add airtime code here
-            airtime.send(options).catch(console.error);
-        }, 5000);
+
     } else {
         response = "END Something must have gone wrong. Please redial the USSD to get started";
         // TODO: Send SMS apologizing for a less than great experience
